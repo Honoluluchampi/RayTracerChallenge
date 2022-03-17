c49c5e611f117e588aa80b5138ecfae367ded0ca
Honoluluchampi <msd13aj@gmail.com>
2022-03-17 15:40:22 +0900

    ifdef transform

--- include/transformation.hpp
+++ include/transformation.hpp
@@ -1,24 +1,31 @@
 #pragma once
-#include <glm_inc.hpp>
-#include <functional>
 
 // all functions should be executed from left to right
 // is it guaranteed by compiler...?
 
-namespace renderer{
-using glm::mat4x4;
-using glm::vec4;
+#ifdef RENDERER_INCLUDE_EIGEN
 
-enum class direction {x, y, z};
+    #include <eigen_inc.hpp>
 
-// for construct
-mat4x4 translation_ctor(const double& x, const double& y, const double& z);
-mat4x4 scaling_ctor(const double& x, const double& y, const double& z);
-mat4x4 reflection_ctor(direction dir);
-mat4x4 rotationX_ctor(const double& rad);
-mat4x4 rotationY_ctor(const double& rad);
-mat4x4 rotationZ_ctor(const double& rad);
+    namespace renderer{
+    using glm::mat4x4;
+    using glm::vec4;
 
-// aP2b : a in proportion to b
-mat4x4 shearing_ctor(const double& xP2y, const double& xP2z, const double& yP2x, const double& yP2z, const double& zP2x, const double& zP2y);
-} // the end of the namespace renderere
+    enum class direction {x, y, z};
+
+    // for construct
+    mat4x4 translation_ctor(const double& x, const double& y, const double& z);
+    mat4x4 scaling_ctor(const double& x, const double& y, const double& z);
+    mat4x4 reflection_ctor(direction dir);
+    mat4x4 rotationX_ctor(const double& rad);
+    mat4x4 rotationY_ctor(const double& rad);
+    mat4x4 rotationZ_ctor(const double& rad);
+
+    // aP2b : a in proportion to b
+    mat4x4 shearing_ctor(const double& xP2y, const double& xP2z, const double& yP2x, const double& yP2z, const double& zP2x, const double& zP2y);
+    } // the end of the namespace renderere
+#else
+
+    #include <glm_inc.hpp>
+    
+#endif
